d93c3ee5d5cdc5e04617f06bcc1d58823d428e79
bit-png <bit-png>
2021-03-01 00:24:56 -0600

    Switch from uglify-es to uglify-js

--- build.js
+++ build.js
@@ -1,9 +1,9 @@
-var UglifyJS = require("uglify-es");
+var UglifyJS = require("uglify-js");
 var fs = require('fs');
 codeString = fs.readFileSync('xenon.js').toString();
 var result = UglifyJS.minify(codeString, {});
 
 if (result.error) console.log(result.error); // runtime error, or `undefined` if no error
-const replace1 = result.code.replace(/\$\.new=\((.*?)\),\$\.all/, '$.new=$1,$.all');
-const replace2 = replace1.replace(/\$\.all=\((.*)\);$/, '$.all=$1');
-fs.writeFileSync('xe.js', replace2);
+//const replace1 = result.code.replace(/\$\.new=\((.*?)\),\$\.all/, '$.new=$1,$.all');
+//const replace2 = replace1.replace(/\$\.all=\((.*)\);$/, '$.all=$1');
+fs.writeFileSync('xe.js', result.code);
